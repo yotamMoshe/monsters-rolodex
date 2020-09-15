1299ae7af9543b5ad08c652b8e34898f6f56c903
יותם <yotammoshe7@gmail.com>
2020-09-15 13:17:24 +0300

    Updates

---
+++ precache-manifest.4b0fa06ccbc7e6c06fcb55e8b394b6ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443f504bf10c676bbbfdd32df1fcff8a",
+    "url": "/https//yotamMoshe.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7faf4356e5605a1476e2",
+    "url": "/https//yotamMoshe.github.io/monsters-rolodex/static/css/main.d71f4a7c.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/https//yotamMoshe.github.io/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/https//yotamMoshe.github.io/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7faf4356e5605a1476e2",
+    "url": "/https//yotamMoshe.github.io/monsters-rolodex/static/js/main.ba3b5e3c.chunk.js"
+  },
+  {
+    "revision": "8d387b8307e18fea91ec",
+    "url": "/https//yotamMoshe.github.io/monsters-rolodex/static/js/runtime-main.b364f023.js"
+  }
+]);
